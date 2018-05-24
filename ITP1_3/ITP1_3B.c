1448a1be556e1c129f821e7eef345552be3c6b61
s1260036 <s1260036@u-aizu.ac.jp>
2018-05-24 18:25:13 +0900

    solve B

---
+++ ITP1_3/ITP1_3B.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+  int i,x;
+  for( i = 0; i < 10000; i++){
+    scanf("%d", &x);
+    if ( x == 0 ) break;
+    printf("Case %d: %d\n", i+1, x);
+  }
+  return 0;
+}
+    
